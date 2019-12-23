8b3f612e6002c97330e24405bdcce7a143d85a17
Luciano Júnior <luciannojunior@gmail.com>
2019-12-23 20:09:42 -0300

    Adding a species of runner

--- src/aoc_2019/core.clj
+++ src/aoc_2019/core.clj
@@ -1,7 +1,16 @@
 (ns aoc-2019.core
+  (:require [aoc-2019.day1 :as day1])
   (:gen-class))
+
+(def funcoes (hash-map :1 day1/run))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [day (first args)] 
+    (println (str "Running the code for day " (first args)))
+    (((keyword day) funcoes))))
+    
+    
+
+  
